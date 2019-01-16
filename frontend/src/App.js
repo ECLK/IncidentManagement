298419117049ec947f4bec67e93eb6acdcd0eac7
Dehan <dehandecroos@gmail.com>
2019-01-16 21:37:43 +0530

    INIT: Adding material js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Button from '@material-ui/core/Button'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello world          
+        <Button variant="contained" color="secondary" >
+          Hello World
+        </Button>     
       </div>
     );
   }
