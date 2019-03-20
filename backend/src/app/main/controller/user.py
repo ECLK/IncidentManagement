b77e3cb9ee12af9376149854744bd9ee522e12bd
Achala Dissanayake <aidissanayake@gmail.com>
2019-03-20 20:03:20 +0530

    user permission level change

--- backend/src/app/main/controller/user.py
+++ backend/src/app/main/controller/user.py
@@ -4,11 +4,7 @@ import enum
 from flask import Flask, jsonify
 from flask_restful import reqparse, abort, Api, Resource, request, fields, marshal_with
 from .. import api
-
-class PermissionLevel(enum.Enum):
-    NOT_ALLOWED = 1  
-    ALLOWED_WITH_APPROVAL = 2
-    ALLOWED = 3
+from ..model.user import PermissionLevel
 
 user_claims = {
     'supervisor'    :   {
