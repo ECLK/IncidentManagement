4b6340d206fbd6143a43fa47878e5d89f8aa9cd7
lahiru94 <alcdealwis@gmail.com>
2019-03-20 12:43:01 +0530

    immutibiliy update untility added

--- frontend/src/modules/incident-filing/state/IncidentFiling.reducers.js
+++ frontend/src/modules/incident-filing/state/IncidentFiling.reducers.js
@@ -1,3 +1,6 @@
+
+import produce from "immer"
+
 import {
     INCIDENT_BASIC_DATA_SUBMIT_REQUEST,
     INCIDENT_BASIC_DATA_SUBMIT_SUCCESS,
@@ -21,30 +24,25 @@ export default function incidentReducer(state, action) {
     if (typeof state === 'undefined') {
         return initialState
     }
-    let newState = {}
-    switch (action.type) {
-        case INCIDENT_STEPPER_FORWARD:
-            newState = JSON.parse(JSON.stringify(state)); //deep copy state object. prolly need a better way
-            newState.guestIncidentForm.activeStep = state.guestIncidentForm.activeStep + 1;
-            return newState;
-        case INCIDENT_STEPPER_BACKWARD:
-            newState = JSON.parse(JSON.stringify(state));
-            newState.guestIncidentForm.activeStep = state.guestIncidentForm.activeStep - 1;
-            return newState;
-        case INCIDENT_BASIC_DATA_SUBMIT_REQUEST:
-            newState = JSON.parse(JSON.stringify(state));
-            newState.guestIncidentForm.stepOneSubmission.inProgress = true;
-            return newState;
-        case INCIDENT_BASIC_DATA_SUBMIT_SUCCESS:
-            newState = JSON.parse(JSON.stringify(state));
-            newState.guestIncidentForm.stepOneSubmission.inProgress = false;
-            return newState;
-        case INCIDENT_BASIC_DATA_SUBMIT_ERROR:
-            newState = JSON.parse(JSON.stringify(state));
-            newState.guestIncidentForm.stepOneSubmission.inProgress = false;
-            return newState
-        default:
-            return state
-
-    }
+    return produce(state, draft => {
+        switch (action.type) {
+            case INCIDENT_STEPPER_FORWARD:
+                draft.guestIncidentForm.activeStep = state.guestIncidentForm.activeStep + 1;
+                return draft
+            case INCIDENT_STEPPER_BACKWARD:
+                draft.guestIncidentForm.activeStep = state.guestIncidentForm.activeStep - 1;
+                return draft
+            case INCIDENT_BASIC_DATA_SUBMIT_REQUEST:
+                draft.guestIncidentForm.stepOneSubmission.inProgress = true;
+                return draft
+            case INCIDENT_BASIC_DATA_SUBMIT_SUCCESS:
+                draft.guestIncidentForm.stepOneSubmission.inProgress = false;
+                return draft
+            case INCIDENT_BASIC_DATA_SUBMIT_ERROR:
+                draft.guestIncidentForm.stepOneSubmission.inProgress = false;
+                return draft
+        }
+    })
 }
+
+
