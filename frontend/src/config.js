cfc8b671340184e276929bcccbb115db7b463b8f
manzzup <manujith.nc@gmail.com>
2019-04-03 22:31:34 +0530

    fixed the API base issue

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,11 @@
 
 //change this to the correct url later
-export const API_BASE_URL = window._env_.API_BASE || "http://localhost:5000";
+var API_BASE_URL = "http://localhost:5000";
+
+if(window._env_ && window._env_.API_BASE_URL){
+    API_BASE_URL = window._env_.API_BASE;
+}
+
+export {
+    API_BASE_URL  
+} 
