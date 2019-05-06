87d86596785ab5de91ad736a5beb7ecbac6403c0
mrclemrkz <awesomeisme007@gmail.com>
2019-05-06 23:57:52 +0530

    make the full card clickable.

--- frontend/src/components/CardGrid/index.js
+++ frontend/src/components/CardGrid/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import { Grid } from "@material-ui/core";
 import './CardGrid.css';
 function CardGrid(props) {
@@ -10,11 +8,7 @@ function CardGrid(props) {
     let getCards = () => {
         let cards = cardContents.map((cardContent, index)=>
             <Grid key={index} item xs={12} sm={4} className='grid-cell'>
-                <Card className='card'>
-                    <CardContent>
-                        {cardContent}
-                    </CardContent>
-                </Card>
+                {cardContent}
             </Grid>
         )
         return cards;
