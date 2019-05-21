61972f7ce6538e96e43fdd83b21b718d742945b3
Achala Dissanayake <aidissanayake@gmail.com>
2019-05-21 16:52:19 +0530

    incidents - mock API

--- frontend/src/api/incident.js
+++ frontend/src/api/incident.js
@@ -1,21 +1,51 @@
-import handler from './apiHandler'
+// import handler from './apiHandler'
+
+// export const createIncident = async (incidentData) => {
+//     return handler.post('/incidents', incidentData);
+// }
+
+// export const getIncident = async (incidentId) => {
+//     return handler.get(`/incidents/${incidentId}`);
+// }
+
+// export const getReporter = async (reporterId) => {
+//     return handler.get(`/reporters/${reporterId}`);
+// }
+
+// export const updateReporter = async (reporterId, reporterData) => {
+//     return handler.put(`/reporters/${reporterId}`, reporterData);
+// }
+
+// export const updateIncident = async (incidentId, incidentData) => {
+//     return handler.put(`/incidents/${incidentId}`, incidentData);
+// }
+
+
+/*
+* mock API for Incidents
+*/
+
+import * as mockapi from '../data/mockapi';
 
 export const createIncident = async (incidentData) => {
-    return handler.post('/incidents', incidentData);
-}
+    await mockapi.addIncident(incidentData);
+    return true;
+};
 
 export const getIncident = async (incidentId) => {
-    return handler.get(`/incidents/${incidentId}`);
-}
+    return mockapi.getIncident(incidentId);
+};
 
-export const getReporter = async (reporterId) => {
-    return handler.get(`/reporters/${reporterId}`);
-}
-
-export const updateReporter = async (reporterId, reporterData) => {
-    return handler.put(`/reporters/${reporterId}`, reporterData);
-}
+export const getIncidents = async () => {
+    return mockapi.getIncidents();
+};
 
 export const updateIncident = async (incidentId, incidentData) => {
-    return handler.put(`/incidents/${incidentId}`, incidentData);
-}
+    return mockapi.updateIncident(incidentId, incidentData);
+};
+
+export const getReporter = async (reporterId) => {
+    return {reporter: "Dummy Reporter"};
+};
+
+export const updateReporter = async (reporterId, reporterData) => {};
--- frontend/src/data/mockapi.js
+++ frontend/src/data/mockapi.js
@@ -1,10 +1,13 @@
 import { events } from './events';
+import { incidents } from './incidents';
+
+const uuidv4 = require('uuid/v4');
 
 export function getEvents(){
     return {
         data: events
     };
-}
+};
 
 export function addComment(commentObj){
     events.push({
@@ -24,4 +27,65 @@ export function addComment(commentObj){
         created_date: Date(),
         approved_date: Date()
     });
+};
+
+export function getIncident(incidentID){
+    let incident = incidents.find(inc => {
+        return inc.id === incidentID;
+    })
+    return {
+        status : 200,
+        data: incident
+    };
 }
+
+export function getIncidents(){
+    return {
+        status : 200,
+        data: incidents
+    };
+};
+
+export function addIncident(incidentData){
+    const incident_id = uuidv4();
+    incidents.push({
+        id: incident_id,
+        ref_ID: incidentData.ref_ID,
+        user_ID: 0,
+        title: incidentData.title,
+        description: incidentData.description,
+        occurrence: incidentData.occurrence,
+        election: incidentData.election,
+        category: incidentData.category,
+        sub_category: incidentData.sub_category,
+        info_channel: incidentData.info_channel,
+        date: incidentData.date
+    });
+
+    events.push({
+        initiator: {
+            isAnonymous: true
+        },
+        action: "CREATED",
+        incidentId: incident_id,
+        created_date: Date()
+    });
+
+    return { status: 200 }
+};
+
+export function updateIncident(incidentId, incidentData){
+    const incidentIndex = incidents.findIndex(inc => inc.id === incidentId);
+    incidents[incidentIndex] = incidentData;
+
+    events.push({
+        initiator: {
+            isAnonymous: true
+        },
+        action: "GENERIC_UPDATE",
+        incidentId: incidentData.id,
+        created_date: Date()
+    });
+
+    return { status: 200 }
+};
