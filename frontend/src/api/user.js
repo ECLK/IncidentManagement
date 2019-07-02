8b72aff01307c45f6572e7de774900d58931d9ef
manzzup <manujith.nc@gmail.com>
2019-07-02 22:46:25 +0530

    assinee

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,7 +2,8 @@ import handler from './apiHandler';
 import * as mockapi from '../data/mockapi';
 
 export const getAllUsers = async () => {
-    return mockapi.getUsers();
+    // return mockapi.getUsers();
+    return (await handler.get(`/users/`)).data;
 }
 
 export const signIn = async (username, password) => {
