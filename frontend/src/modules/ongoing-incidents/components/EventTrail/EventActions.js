2728e078097adbd75ab101b9ea4dac529e26ec8a
lahiru94 <alcdealwis@gmail.com>
2019-07-16 10:40:47 +0530

    UI changes

--- frontend/src/modules/ongoing-incidents/components/EventTrail/EventActions.js
+++ frontend/src/modules/ongoing-incidents/components/EventTrail/EventActions.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import StatusChange from './StatusChange';
 import Assignees from '../Assignees';
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
-
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import Moment from 'react-moment';
 import { getDateDiff } from './utils';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+
+//icons
+import RestoreIcon from '@material-ui/icons/Restore';
+import TimerIcon from '@material-ui/icons/Timer';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
+import SubdirectoryArrowLeftIcon from '@material-ui/icons/SubdirectoryArrowLeft'
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import HelpIcon from '@material-ui/icons/Help';
+import EditIcon from '@material-ui/icons/Edit';
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 
 const styles = (theme) => ({
     card: {
@@ -21,13 +40,17 @@ const styles = (theme) => ({
     button: {
         marginTop: theme.spacing.unit,
     },
-    divider:{
-        marginTop: theme.spacing.unit*4
+    divider: {
+        marginTop: theme.spacing.unit * 4
     },
     topDivider: {
         marginTop: theme.spacing.unit
+    },
+    actionButtonIcon:{
+        marginRight: theme.spacing.unit * 2
     }
 });
+
 
 class EventActions extends React.Component {
 
@@ -58,10 +81,15 @@ class EventActions extends React.Component {
             onSeverityChange, activeUser, getUsers,
             setIncidentAssignee, users } = this.props;
 
+        var hourlyResponseTimes = []
+        for (var i = 1; i < 24; i++) {
+            hourlyResponseTimes.push(i);
+        }
+
         return (
             <div className={classes.card}>
 
-                <Divider variant="middle" className={classes.topDivider}/>
+                <Divider variant="middle" className={classes.topDivider} />
 
 
                 <Typography variant="h6" style={{ marginTop: "20px" }}>
@@ -77,13 +105,6 @@ class EventActions extends React.Component {
                 />
 
                 {/* <Typography variant="h6" style={{ marginTop: "20px" }}>
-                    Logged Date and Time
-                </Typography>
-                <Typography variant="h7">
-                    <Moment>{activeIncident.createdDate}</Moment>
-                </Typography> */}
-
-                <Typography variant="h6" style={{ marginTop: "20px" }}>
                     Time from last action
                     </Typography>
                 <Typography variant="h7">
@@ -93,9 +114,22 @@ class EventActions extends React.Component {
                 <Typography variant="h6" style={{ marginTop: "20px" }}>
                     Required response time
                 </Typography>
-                <Typography variant="h7">
-                    6 hours
-                </Typography>
+                <FormControl className={classes.formControl}>
+                    <Select
+                        value={activeIncident.responseTimeInHours}
+                        onChange={this.handleChange}
+                        name="age"
+                        displayEmpty
+                        className={classes.selectEmpty}
+                    >
+                        <MenuItem value="" disabled>
+                            N/A
+                        </MenuItem>
+                        {hourlyResponseTimes.map((value, index)=>{
+                            return (<MenuItem key={index} value={value}>{value}</MenuItem>)
+                        })}
+                    </Select>
+                </FormControl>
 
                 <Typography variant="h6" style={{ marginTop: "20px" }}>
                     Status:
@@ -103,13 +137,6 @@ class EventActions extends React.Component {
                 <Typography variant="h7">
                     {activeIncident.status}
                 </Typography>
-                {/* <StatusChange 
-                        activeIncident={activeIncident}
-                        currentValue={activeIncident.status} 
-                        onValueChange={onStatusChange}
-                        selectType="status"
-                        activeUser={activeUser}
-                    /> */}
 
                 <Typography variant="h6" style={{ marginTop: "20px" }}>
                     Severity
@@ -120,18 +147,70 @@ class EventActions extends React.Component {
                     onValueChange={onSeverityChange}
                     selectType="severity"
                     activeUser={activeUser}
-                />
+                /> */}
 
-                
-                <Divider variant="middle" className={classes.divider}/>
+
+                <List className={classes.root}>
+                    <ListItem>
+                        <Avatar>
+                            <RestoreIcon />
+                        </Avatar>
+                        <ListItemText primary="Time Since last action" secondary={this.getLastActionTime()} />
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <TimerIcon />
+                        </Avatar>
+                        <ListItemText primary="Required response time" secondary={activeIncident.responseTimeInHours+" hours.1 hour(s) remaining.\n Ends at 5.30 p.m."} />
+
+                        <ListItemSecondaryAction>
+                            {/* <Select
+                            value={activeIncident.responseTimeInHours}
+                            onChange={this.handleChange}
+                            name="age"
+                            displayEmpty
+                            className={classes.selectEmpty}
+                            >
+                                <MenuItem value="" disabled>
+                                    N/A
+                                </MenuItem>
+                                {hourlyResponseTimes.map((value, index)=>{
+                                    return (<MenuItem key={index} value={value}>{value}</MenuItem>)
+                                })}
+                            </Select> */}
+                            <IconButton aria-label="Edit">
+                                <EditIcon/>
+                            </IconButton>
+                        </ListItemSecondaryAction>
+
+                    </ListItem>
+                    <ListItem>
+                        <Avatar>
+                            <ShowChartIcon />
+                        </Avatar>
+                        <ListItemText primary="Status" secondary={activeIncident.status} />
+                    </ListItem>
+                </List>
+
+
+                <Divider variant="middle" className={classes.divider} />
 
                 {/* <Typography variant="h6" style={{ marginTop: "20px" }}>
                     Actions
                 </Typography> */}
 
-                <Button color="primary" size="large" variant='text' className={classes.button} onClick={this.props.escallateIncident}>Escalate</Button>
-                <Button color="primary" size="large" variant='text' className={classes.button}>Escalate to outside </Button>
-                <Button color="primary" size="large" variant='text' className={classes.button}>Request for advice</Button>
+                <Button color="primary" size="large" variant='text' className={classes.button} onClick={this.props.escallateIncident}>
+                    <ArrowUpwardIcon className={classes.actionButtonIcon}/>
+                    Escalate
+                </Button>
+                <Button color="primary" size="large" variant='text' className={classes.button}>
+                    <SubdirectoryArrowLeftIcon className={classes.actionButtonIcon}/>
+                    Escalate to outside 
+                </Button>
+                <Button color="primary" size="large" variant='text' className={classes.button}>
+                    <HelpIcon className={classes.actionButtonIcon}/>
+                    Request for advice
+                </Button>
 
 
             </div>
