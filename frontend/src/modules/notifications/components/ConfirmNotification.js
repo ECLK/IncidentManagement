481ff4a4499184322ba546dea6413059e2140b55
manzzup <manujith.nc@gmail.com>
2019-08-07 19:32:40 +0530

    frontend docker fixed for production

---
+++ frontend/src/modules/notifications/components/ConfirmNotification.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Notification from './Notification';
+import { useSelector } from 'react-redux';
+
+const ConfirmNotification = (props) => {
+    const confirms = useSelector(state => state.notificationReducer.confirms);
+    
+    return (
+        <>
+            {confirms && (
+                <Notification
+                    notificationType="success"
+                    message={confirms.message}
+                />
+            )}
+        </>
+    )
+}
+
+export default ConfirmNotification;
