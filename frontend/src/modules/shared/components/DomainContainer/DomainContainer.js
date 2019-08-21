b7cc939ea4580a9184d313a2f7fdc313f100815f
lahiru94 <alcdealwis@gmail.com>
2019-08-21 20:46:55 +0530

    Breadcrumbs done

--- frontend/src/modules/shared/components/DomainContainer/DomainContainer.js
+++ frontend/src/modules/shared/components/DomainContainer/DomainContainer.js
@@ -16,16 +16,17 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-import {initiateSignOut} from '../../state/Shared.actions'
-import {changeLanguage} from '../../state/Shared.actions';
+import { initiateSignOut } from '../../state/Shared.actions'
+import { changeLanguage } from '../../state/Shared.actions';
 
 import RootModal from '../../../modals/components/RootModal'
 import Notification from '../../../notifications/components/Notification';
 import ErrorNotification from '../../../notifications/components/ErrorNotification';
 import ConfirmNotification from '../../../notifications/components/ConfirmNotification';
 import LoadingNotification from '../../../notifications/components/LoadingNotification';
+import Breadcrumbs from '../Breadcrumbs'
 
 const HomeLink = props => <Link to="/app/home" {...props} />
 const ReportLink = props => <Link to="/app/report" {...props} />
@@ -35,222 +36,227 @@ const ReviewLink = props => <Link to="/app/review" {...props} />
 const drawerWidth = 240;
 
 const styles = theme => ({
-  root: {
-    // display: 'flex',
-    flexGrow: 1
-  },
-  homeButton: {
-    marginLeft: theme.spacing.unit*4
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 20,
-  },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    paddingTop: theme.spacing.unit * 3,
-    paddingLeft: theme.spacing.unit * 8,
-    paddingRight: theme.spacing.unit * 8,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginTop: theme.spacing.unit*4
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-  grow: {
-    flexGrow: 1,
-  },
+    root: {
+        // display: 'flex',
+        flexGrow: 1
+    },
+    homeButton: {
+        marginLeft: theme.spacing.unit * 4
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 20,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        paddingTop: theme.spacing.unit * 3,
+        paddingLeft: theme.spacing.unit * 8,
+        paddingRight: theme.spacing.unit * 8,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginTop: theme.spacing.unit * 1
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    breadCrumbWrapper: {
+        marginBottom: theme.spacing.unit * 4
+    }
 });
 
 class DomainContainer extends React.Component {
-  state = {
-    open: true,
-    anchorEl: null,
-    anchorLang: null
-  };
+    state = {
+        open: true,
+        anchorEl: null,
+        anchorLang: null
+    };
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
 
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
 
-  handleLangMenu = event => {
-    this.setState({ anchorLang: event.currentTarget });
-  };
+    handleLangMenu = event => {
+        this.setState({ anchorLang: event.currentTarget });
+    };
 
-  handleLangMenuClose = () => {
-    this.setState({ anchorLang: null });
-  };
+    handleLangMenuClose = () => {
+        this.setState({ anchorLang: null });
+    };
 
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
+    handleMenu = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
-  };
+    handleMenuClose = () => {
+        this.setState({ anchorEl: null });
+    };
 
-  handleSignOut = () => {
-      const {signOut} = this.props;
-      signOut();
-  }
+    handleSignOut = () => {
+        const { signOut } = this.props;
+        signOut();
+    }
 
-  handleLanguageChange = (lang) => {
-    const {changeLanguage} = this.props;
-    changeLanguage(lang);
-  }
+    handleLanguageChange = (lang) => {
+        const { changeLanguage } = this.props;
+        changeLanguage(lang);
+    }
 
-  render() {
-    const { classes, selectedLanguage, signedInUser } = this.props;
-    const { open, anchorEl, anchorLang } = this.state;
-    const menuOpen = Boolean(anchorEl);
-    const langMenuOpen = Boolean(anchorLang);
-
-    return (
-      <div className={classes.root}>
-        <ErrorNotification />
-        <ConfirmNotification />
-        <LoadingNotification />
-        <CssBaseline />
+    render() {
+        const { classes, selectedLanguage, signedInUser, location } = this.props;
+        const { open, anchorEl, anchorLang } = this.state;
+        const menuOpen = Boolean(anchorEl);
+        const langMenuOpen = Boolean(anchorLang);
 
 
-        <AppBar
-          position="static"
+        return (
+            <div className={classes.root}>
+                <ErrorNotification />
+                <ConfirmNotification />
+                <LoadingNotification />
+                <CssBaseline />
 
-        >
-          <Toolbar disableGutters={!open}>
 
-            <Typography variant="h6" color="inherit" className={classes.grow}>
-                Incident Management
+                <AppBar
+                    position="static"
 
-                <Button color="inherit" component={HomeLink} className={classes.homeButton}>Home</Button>
-                <Button color="inherit" component={ReportLink}>Create</Button>
-                <Button color="inherit" component={ReviewLink}>Review</Button>
-
-            </Typography>
-
-            <Button
-                  aria-owns={open ? 'menu-appbar' : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleLangMenu}
-                  color="inherit"
                 >
-            {selectedLanguage}
-            </Button>
-            <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorLang}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={langMenuOpen}
-                  onClose={this.handleLangMenuClose}
-                >
-                  <MenuItem onClick={()=>(this.handleLanguageChange('si'))}>Sinhala</MenuItem>
-                  <MenuItem onClick={()=>(this.handleLanguageChange('ta'))}>Tamil</MenuItem>
-                  <MenuItem onClick={()=>(this.handleLanguageChange('en'))}>English</MenuItem>
-            </Menu>
-            <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={menuOpen}
-                  onClose={this.handleMenuClose}
-                >
-                  <MenuItem >Profile</MenuItem>
-                  <MenuItem onClick={this.handleSignOut}>Sign Out</MenuItem>
-            </Menu>
+                    <Toolbar disableGutters={!open}>
 
-            <Button 
-              aria-owns={open ? 'menu-appbar' : undefined}
-              aria-haspopup="true" 
-              color="inherit" 
-              onClick={this.handleMenu} 
-            > 
-              {signedInUser.displayname}
-              <AccountCircle style={{margin:5}} />
-            </Button>
-
-
+                        <Typography variant="h6" color="inherit" className={classes.grow}>
+                            Incident Management
             
-          </Toolbar>
-        </AppBar>
-        <main
-          className={classNames(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >
-          <RootModal/>
-          
-            {this.props.content}
-          
-        </main>
-      </div>
-    );
-  }
+                            <Button color="inherit" component={HomeLink} className={classes.homeButton}>Home</Button>
+                            <Button color="inherit" component={ReportLink}>Create</Button>
+                            <Button color="inherit" component={ReviewLink}>Review</Button>
+
+                        </Typography>
+
+                        <Button
+                            aria-owns={open ? 'menu-appbar' : undefined}
+                            aria-haspopup="true"
+                            onClick={this.handleLangMenu}
+                            color="inherit"
+                        >
+                            {selectedLanguage}
+                        </Button>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorLang}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={langMenuOpen}
+                            onClose={this.handleLangMenuClose}
+                        >
+                            <MenuItem onClick={() => (this.handleLanguageChange('si'))}>Sinhala</MenuItem>
+                            <MenuItem onClick={() => (this.handleLanguageChange('ta'))}>Tamil</MenuItem>
+                            <MenuItem onClick={() => (this.handleLanguageChange('en'))}>English</MenuItem>
+                        </Menu>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={menuOpen}
+                            onClose={this.handleMenuClose}
+                        >
+                            <MenuItem >Profile</MenuItem>
+                            <MenuItem onClick={this.handleSignOut}>Sign Out</MenuItem>
+                        </Menu>
+
+                        <Button
+                            aria-owns={open ? 'menu-appbar' : undefined}
+                            aria-haspopup="true"
+                            color="inherit"
+                            onClick={this.handleMenu}
+                        >
+                            {signedInUser.displayname}
+                            <AccountCircle style={{ margin: 5 }} />
+                        </Button>
+
+
+                    </Toolbar>
+                </AppBar>
+                <main
+                    className={classNames(classes.content, {
+                        [classes.contentShift]: open,
+                    })}
+                >
+                    <RootModal />
+
+                    <div className={classes.breadCrumbWrapper}><Breadcrumbs pathname={location.pathname}/></div>
+
+                    {this.props.content}
+
+                </main>
+            </div>
+        );
+    }
 }
 
 DomainContainer.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        isSignedIn:state.sharedReducer.signedInUser.isSignedIn,
+        isSignedIn: state.sharedReducer.signedInUser.isSignedIn,
         selectedLanguage: state.sharedReducer.selectedLanguage,
         signedInUser: state.sharedReducer.signedInUser.data
     }
@@ -258,24 +264,24 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        signOut:() => {
+        signOut: () => {
             dispatch(initiateSignOut())
         },
         changeLanguage: (lang) => {
             dispatch(changeLanguage(lang))
         },
         showError: (error) => {
-          dispatch({
-            type: "SHOW_NOTIFICATION",
-            error: {
-              message: "Major error in data! Fallback"
-            }
-          })
+            dispatch({
+                type: "SHOW_NOTIFICATION",
+                error: {
+                    message: "Major error in data! Fallback"
+                }
+            })
         }
     }
 }
 
-export default compose( 
+export default withRouter(compose(
     withStyles(styles, { withTheme: true }),
     connect(mapStateToProps, mapDispatchToProps))
-    (DomainContainer);
+    (DomainContainer));
