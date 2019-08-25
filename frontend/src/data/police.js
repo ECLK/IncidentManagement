9e2d943525ae7e4b2cc3c5c4454c179059819778
mrclemrkz <awesomeisme007@gmail.com>
2019-08-25 22:05:17 +0530

    add missing mock data

---
+++ frontend/src/data/police.js
@@ -0,0 +1,7 @@
+export const police_stations = [
+    { id:1, code:"POL00001", name:"Borella Police"},
+    { id:2, code:"POL00007", name:"Cinnamon Garden Police"},
+    { id:3, code:"POL00012", name:"Rajagiriya Police"},
+    { id:4, code:"POL00101", name:"Wellawatta Police"},
+    { id:5, code:"POL00023", name:"Ingiriya Police"},
+];
---
+++ frontend/src/data/polling.js
@@ -0,0 +1,17 @@
+export const polling_divisions = [
+    { id: 1, code: "PLD220933", name: "Kolonnawa" }, 
+    { id: 2, code: "PLD220934", name: "Homagama" }, 
+    { id: 3, code: "PLD220935", name: "Maharagama" }, 
+    { id: 4, code: "PLD220936", name: "Dehiwala" }, 
+    { id: 5, code: "PLD220937", name: "Rathmalana" }, 
+    { id: 6, code: "PLD220938", name: "Moratuwa" }
+];
+
+export const polling_stations = [
+    { id:1, code: "PLS123101", name:"St. Peter's College" },
+    { id:2, code: "PLS123102", name:"Royal College" },
+    { id:3, code: "PLS123103", name:"St. Thomas College" },
+    { id:4, code: "PLS123104", name:"Ananda College" },
+    { id:5, code: "PLS123105", name:"Isipathana College" },
+    { id:6, code: "PLS123106", name:"Asoka College" },
+];
