22eee482bfcaa81ef6da096db0771b691cb7aef0
Achala Dissanayake <aijdissanayake@gmail.com>
2019-08-26 01:42:37 +0530

    media attached event

--- backend/src/file_upload/views.py
+++ backend/src/file_upload/views.py
@@ -33,7 +33,7 @@ class FileView(APIView):
     file_serializer = FileSerializer(data=file_data)
     if file_serializer.is_valid():
       file_serializer.save()
-      # print (file_serializer.data.id)
+      print (file_serializer.data.id)
       return Response(file_serializer.data, status=status.HTTP_201_CREATED)
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
