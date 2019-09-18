7cb0ee1f34f5f07ea5b905428e5f15f12ef8bf9a
lahiru94 <alcdealwis@gmail.com>
2019-09-18 20:53:42 +0530

    public form minor changes

---
+++ frontend/src/modules/guest-view/components/GuestFormSuccessPage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { withRouter } from "react-router";
+import {useDispatch} from 'react-redux';
+
+
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import {
+    moveStepper
+} from '../state/guestViewActions'
+
+
+function GuestFormSuccessPage(props) {
+
+    const dispatch = useDispatch();
+
+    const handleButtonClick = (action) => {
+        switch(action){
+            case 'edit':
+                props.history.push(`/report`);
+                dispatch(moveStepper(1))
+                break;
+            case 'status':
+                props.history.push(`/status`);
+                break;
+            case 'create':
+                //clear active incident
+                props.history.push(`/report`);
+                break;
+            default:
+                break;
+        }
+        
+    } 
+
+    return (
+        <Grid container spacing={16} style={{marginTop:'13%'}}>
+            <Grid item xs={12}>
+            <h1 style={{display:'flex', flexGrow:1, justifyContent:'center'}}>
+                Your complaint has been submitted successfully
+            </h1>
+            </Grid>
+            <Grid item xs={3}/>
+
+            <Grid item xs={6} style={{marginTop:'30px'}}>
+            <div style={{display:'flex', flexGrow:1, justifyContent:'space-around'}}>
+                <Button variant='outlined' onClick={()=>{handleButtonClick('edit')}}>Add/ edit information</Button>
+                <Button variant='outlined' onClick={()=>{handleButtonClick('status')}}>Check  status</Button>
+                <Button variant='outlined' onClick={()=>{handleButtonClick('create')}}>Create another complaint</Button>
+            </div>
+            </Grid>
+            <Grid item xs={3}/>
+        </Grid>
+    );
+}
+
+export default withRouter(GuestFormSuccessPage);
