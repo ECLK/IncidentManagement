cf9bb6ad6a67b9aad6ea607be51343231a95e3b5
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2019-11-07 10:35:55 +0530

    file upload fixes

--- frontend/src/modules/shared/components/FileUploader.js
+++ frontend/src/modules/shared/components/FileUploader.js
@@ -1,6 +1,10 @@
 import React, { useRef } from 'react';
-import { FilePond } from "react-filepond";
+import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+
+// Register the plugin
+registerPlugin(FilePondPluginFileValidateSize);
 
 export default function FileUploader({ files, setFiles }) {
     const pond = useRef(null);
@@ -10,10 +14,17 @@ export default function FileUploader({ files, setFiles }) {
             ref={pond}
             files={files}
             allowMultiple={true}
+            maxTotalFileSize= '100MB'
             // oninit={() => this.handleInit()}
             onupdatefiles={fileItems => {
                 // Set currently active file objects to this.state
-                setFiles(fileItems.map(fileItem => fileItem.file))
+                let totalFileSize = fileItems.reduce((totalSize,currFile)=>{
+                    return totalSize = totalSize + currFile.fileSize
+                },0)
+                if(totalFileSize<100000000){
+                    setFiles(fileItems.map(fileItem => fileItem.file))
+
+                }
             }}
         />
     )
