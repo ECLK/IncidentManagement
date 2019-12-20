e9e9fcc168f8ab306c07a74b695b68aed8d85ce7
MrClemRkz <clement.amila@gmail.com>
2019-12-21 02:27:55 +0530

    bug fix #375

--- frontend/src/modules/notifications/components/Notification.js
+++ frontend/src/modules/notifications/components/Notification.js
@@ -9,39 +9,39 @@ import { HIDE_NOTIFICATION } from '../state/notifications.type';
 
 const styles = theme => ({
     success: {
-      backgroundColor: green[600],
+        backgroundColor: green[600],
     },
     error: {
-      backgroundColor: theme.palette.error.dark,
+        backgroundColor: theme.palette.error.dark,
     },
     info: {
-      backgroundColor: theme.palette.primary.dark,
+        backgroundColor: theme.palette.primary.dark,
     },
     warning: {
-      backgroundColor: amber[700],
+        backgroundColor: amber[700],
     },
     icon: {
-      fontSize: 20,
+        fontSize: 20,
     },
     iconVariant: {
-      opacity: 0.9,
-      marginRight: theme.spacing.unit,
+        opacity: 0.9,
+        marginRight: theme.spacing.unit,
     },
     message: {
-      display: 'flex',
-      alignItems: 'center',
+        display: 'flex',
+        alignItems: 'center',
     },
 });
 
-const Notification = ({classes, notificationType, message}) => {
+const Notification = ({ classes, notificationType, message }) => {
     const open = useSelector(state => state.notificationReducer.isOpen);
     const dispatch = useDispatch();
 
-    function handleClose(){
+    function handleClose() {
         dispatch({ type: HIDE_NOTIFICATION });
     }
-    
-    return (    
+
+    return (
         <Snackbar
             anchorOrigin={{
                 vertical: 'bottom',
@@ -51,34 +51,33 @@ const Notification = ({classes, notificationType, message}) => {
             autoHideDuration={6000}
             onClose={handleClose}
             ContentProps={{
-            'aria-describedby': 'message-id',
-            
+                'aria-describedby': 'message-id',
             }}
             action={[
                 <CloseIcon className={classes.icon} />
             ]}
         >
             <SnackbarContent
-                    className={classes[notificationType]}
-                    aria-describedby="client-snackbar"
-                    message={
-                        <span id="client-snackbar" className={classes.message}>
+                className={classes[notificationType]}
+                aria-describedby="client-snackbar"
+                message={
+                    <span id="client-snackbar" className={classes.message}>
                         <Icon className={classes.icon} />
-                            {message}
-                        </span>
-                    }
-                    action={[
-                        <IconButton
-                            key="close"
-                            aria-label="Close"
-                            color="inherit"
-                            className={classes.close}
-                            onClick={handleClose}
-                        >
-                            <CloseIcon className={classes.icon} />
-                        </IconButton>,
-                    ]}
-                />
+                        {message}
+                    </span>
+                }
+                action={[
+                    <IconButton
+                        key="close"
+                        aria-label="Close"
+                        color="inherit"
+                        className={classes.close}
+                        onClick={handleClose}
+                    >
+                        <CloseIcon className={classes.icon} />
+                    </IconButton>,
+                ]}
+            />
         </Snackbar>
     );
 }
