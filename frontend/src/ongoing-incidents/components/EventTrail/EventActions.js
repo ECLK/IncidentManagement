19a2fe22237ea50883065cadab9d3632a579b81a
MrClemRkz <clement.amila@gmail.com>
2020-03-13 14:13:27 +0530

    print slip cors fix test

--- frontend/src/ongoing-incidents/components/EventTrail/EventActions.js
+++ frontend/src/ongoing-incidents/components/EventTrail/EventActions.js
@@ -107,7 +107,25 @@ const EventActions = (props) => {
     }
 
     async function printSlip(){
-        const response = (await handler.get(`${API_BASE_URL}/pdfgen/?template_type=slip&id=`+activeIncident.id, {responseType: 'blob'}))
+        const config = {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+                "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+            },
+            responseType: 'blob'
+        }
+        const config2 = {
+            responseType: 'stream'
+        }
+        const config3 = {
+            headers: {
+                'Accept': 'application/pdf',
+            },
+            responseType: 'blob'
+        }
+        const response = (await handler.get(`${API_BASE_URL}/pdfgen/?template_type=slip&id=`+activeIncident.id, config))
         const data = response.data
         const blob = new Blob([data], { type: 'application/pdf' });
         const uri = URL.createObjectURL(blob);
