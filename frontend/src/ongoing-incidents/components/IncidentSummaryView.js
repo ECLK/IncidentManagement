76db9d13a9132c8b7daaba0ad2c5162e38622ad0
YujithIsura <ujithisura@gmail.com>
2020-07-27 13:09:17 +0530

    bug fixed in #557

--- frontend/src/ongoing-incidents/components/IncidentSummaryView.js
+++ frontend/src/ongoing-incidents/components/IncidentSummaryView.js
@@ -151,10 +151,10 @@ function BasicDetailTab(props) {
                                     {categories.map((value, index) => (value.id == incident.category ? value.sub_category : null))}
                                 </Typography>
                             </Grid>
-                            <Grid item xs>
+                            {incident.incidentType !== "INQUIRY" ? <Grid item xs>
                                 <Typography variant="caption" className={classes.label}> Severity </Typography>
                                 <Typography gutterBottom> {incident.severity == 1 || incident.severity == 2 || incident.severity == 3 ? "Low" : incident.severity == 4 || incident.severity == 5 || incident.severity == 6 || incident.severity == 7 ? "Medium" : "High" } </Typography>
-                            </Grid>
+                            </Grid> : ""}
                             {/* <Grid item xs>
                                 <Typography variant="caption" className={classes.label}> Sub Category </Typography>
                                 <Typography gutterBottom> {this.state.category.sub_category} </Typography>
